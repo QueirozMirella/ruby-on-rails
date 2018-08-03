9d542b2faaab43f656e5f5e864fd4a548748c3a1
Mirella Queiroz <queiroz.av@gmail.com>
2018-08-03 15:10:12 -0300

    criando os models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cadastrando as categorias"
+
+categories = [ "Animais e acessórios",
+				"Esportes",
+				"Para sua casa",
+				"Eletrônicos e celulares",
+				"Música e hobbies",
+				"Bebês e crianças",
+				"Moda e beleza",
+				"Veículos e barcos",
+				"Imóveis",
+				"Empregos e negócios" ]
+
+categories.each do |category|
+	Category.find_or_create_by(description: category)
+end
+
+puts "cadastrado com sucesso"
